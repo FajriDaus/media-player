1bb85415a3158b9d021dc59c5e4d0b987a30b6e0
FajriDaus <fajrifirdaus47@gmail.com>
2022-09-01 00:32:51 +0700

    Updates

---
+++ precache-manifest.501a6cf2bdae124b7436c2ad6a762996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de89f11966b7f846a464f68620f73ee",
+    "url": "/media-player/index.html"
+  },
+  {
+    "revision": "db87a12d057fd2e2c26e",
+    "url": "/media-player/static/css/main.7685ed08.chunk.css"
+  },
+  {
+    "revision": "3aa895cd5cc1f2d7e2d5",
+    "url": "/media-player/static/js/2.87671071.chunk.js"
+  },
+  {
+    "revision": "efc6b2bb720c0b9036a8eb8f5fd24b70",
+    "url": "/media-player/static/js/2.87671071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db87a12d057fd2e2c26e",
+    "url": "/media-player/static/js/main.c3882ec5.chunk.js"
+  },
+  {
+    "revision": "695cd8cb465794116741",
+    "url": "/media-player/static/js/runtime-main.d8365ff9.js"
+  }
+]);
