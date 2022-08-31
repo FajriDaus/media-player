1aa02c2ad07b0a2901f8f79753bf1331a24710cc
FajriDaus <fajrifirdaus47@gmail.com>
2022-09-01 00:49:38 +0700

    check all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import PlaylistPage from "./pages/PlaylistPage";
+import PlayingPage from "./pages/PlayingPage";
+import HomePage from "./pages/HomePage";
+import BottomNavigationComponent from "./components/BottomNavigation";
+import { SongProvider } from "./context";
 
 function App() {
+  // Persiapkan nilai awalnya
+  const initialState = {
+    thumbnailSrc: "",
+    songSrc: "",
+    name: "",
+    artistName: "",
+    trackId: "",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Kita gunakan context dengan value yang akan digunakan di komponen-komponen
+    // Challenge: Setiap di-refresh, state kita akan kembali ke value. Bagaimana caranya kalau setiap refresh
+    // value state kita masih tetap ada?
+    // Clue: gunakan localStorage dari browser untuk simpan nilai state (tanpa Backend)
+    <SongProvider initialSong={initialState}>
+      {/* Kita gunakan Router agar setiap URL yang pengguna buka akan merender halaman yang diinginkan */}
+      <Router>
+        {/* Daftar rute (<Route />) yang kita gunakan taruh di <Switch /> */}
+        <Switch>
+          <Route path="/playing">
+            <PlayingPage />
+          </Route>
+          <Route path="/playlist">
+            <PlaylistPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+        {/* Komponen navigasi yang akan digunakan */}
+        <BottomNavigationComponent />
+      </Router>
+    </SongProvider>
   );
 }
 
